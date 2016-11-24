028e9afd8c6a4b969657f882dc06fa112d01c9fd
Christian Bach <christian.bach@th-wildau.de>
2016-11-24 17:27:36 +0100

    Sicherungs der Shell-Scripts

---
+++ Jarvis/record.sh
@@ -0,0 +1,10 @@
+
+echo "Start Recording..."
+arecord -D plughw:1,0 -f cd -t wav -d 5 -r 16000 -c 1 | flac - -f --sample-rate 16000 -o out.flac
+echo "Stop Recording!"
+
+# Encoding Audio-File
+echo "Encoding Audio-File..."
+base64 out.flac -w 0 > audio.json
+echo "Done."
+
